9c3f9a43d32ab66e37e00740deffdaec94b15cc8
Ahmed200011 <ahmed.w632@yahoo.com>
2025-06-18 23:03:51 +0300

    new file:   app/Helpers/ApiResponse.php 	new file:   app/Http/Controllers/Api/AuthenticationController.php 	new file:   app/Http/Requests/RegisterRequest.php 	new file:   app/Http/Resources/UserResource.php 	modified:   app/Models/User.php 	modified:   bootstrap/app.php 	modified:   composer.json 	modified:   composer.lock 	new file:   config/sanctum.php 	new file:   database/migrations/2025_06_17_191146_create_personal_access_tokens_table.php 	new file:   routes/api.php

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\Api\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(AuthenticationController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+});
+
+// Route::post('/login', [AuthenticationController::class, 'login']);
+// Route::get('/l', [BookController::class, 'index']);
